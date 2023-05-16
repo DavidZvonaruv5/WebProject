d5e7910531c4fa20d1f2bf91981417c0ca407fc7
DavidZvonaruv5 <102156879+DavidZvonaruv5@users.noreply.github.com>
2023-05-16 14:28:23 +0300

    Changes to cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,13 +1,15 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: (origin,callback) =>{
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null,true)
-        } else{
-            callback(new Error('not allowed by CORS'))
-        }
-    },
+    origin: '*',
+    //the origin will accpet cors from anywhere.
+    // origin: (origin,callback) =>{
+    //     if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    //         callback(null,true)
+    //     } else{
+    //         callback(new Error('not allowed by CORS'))
+    //     }
+    // },
     credentials: true,
     optionsSuccessStatus: 200
 }
