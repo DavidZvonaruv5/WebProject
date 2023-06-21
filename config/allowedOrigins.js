5aedbe5a34719225fa717db1fcdb8e9e89be822c
DavidZvonaruv5 <102156879+DavidZvonaruv5@users.noreply.github.com>
2023-06-21 13:00:27 +0300

    deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 
 //these are the allowed sites that can fetch data and not be blocked by cors.
 const allowedOrigins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://taskflow.onrender.com',
 ]
 
 module.exports = allowedOrigins
